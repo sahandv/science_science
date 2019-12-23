f98fa75c1ceeee5982080941995d69e1ed4c43b0
Sahand V <sahand.vahidnia@yahoo.com>
2019-12-23 18:13:05 +1100

    minor change

--- Cluster Quality Check -- draft.py
+++ Cluster Quality Check -- draft.py
@@ -51,11 +51,13 @@ from sciosci.assets import keyword_assets as kw
 from sciosci.assets import generic_assets as sci
 from sciosci.assets import advanced_assets as aa
 
+period_cluster = '2017-2018 13'
+
 # Read cluster centers
-cluster_centers = pd.read_csv('/home/sahand/GoogleDrive/Data/FastText doc clusters - SIP/50D/cluster_centers/agglomerative ward 1990-2004 7',index_col=0)
+cluster_centers = pd.read_csv('/home/sahand/GoogleDrive/Data/FastText doc clusters - SIP/50D/cluster_centers/agglomerative ward '+period_cluster,index_col=0)
 
 # Read and make keyword list
-keywords = pd.read_csv('/home/sahand/GoogleDrive/Data/Author keywords - 02 Nov 2019/2017-2018 keyword frequency',names=['keyword','frequency'])
+keywords = pd.read_csv('/home/sahand/GoogleDrive/Data/Author keywords - 29 Oct 2019/1990-2018 keyword frequency',names=['keyword','frequency'])
 keywords = keywords[keywords['frequency']>20]
 keywords_list = keywords['keyword'].values.tolist()
 # Get keyword embeddings
@@ -69,23 +71,25 @@ for token in tqdm(keywords_list[:],total=len(keywords_list[:])):
 
 # Cosine distance of the cluster centers and keywords to find the closest keywords to clusters
 names = []
-names.append('cluster_1')
+names.append('clusters')
 sim_A_to_B = []
 for idx_A,vector_A in cluster_centers.iterrows():
     inner_similarity_scores = []
     inner_similarity_scores.append(idx_A)
     for idx_B,vector_B in enumerate(keyword_vectors):
         distance_tmp = spatial.distance.cosine(vector_A.values, vector_B)
-        similarity_tmp = 1 - distance_tmp
+        similarity_tmp = distance_tmp#1 - distance_tmp
 
-        inner_similarity_scores.append(idx_B)
+#        inner_similarity_scores.append(keywords_list[idx_B])
         inner_similarity_scores.append(similarity_tmp)
 
         if idx_A == 0:
-            names.append('cluster_2_'+str(idx_B))
-            names.append('similarity_'+str(idx_B))
+#            names.append('keyword_'+str(idx_B))
+            names.append(keywords_list[idx_B])
 
     sim_A_to_B.append(inner_similarity_scores)
         # print('cluster of A:',idx_A,'to cluster of B:',idx_B,'similarity',similarity_tmp)
 
 sim_A_to_B = pd.DataFrame(sim_A_to_B,columns=names)
+
+sim_A_to_B.to_csv('/home/sahand/GoogleDrive/Data/FastText doc clusters - SIP/50D/cluster_center to author_keyword similarity/'+period_cluster+' - distance.csv',index=False)
