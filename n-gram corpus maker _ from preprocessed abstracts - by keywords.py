2535ff18729ba215bbb36596eb596a1752f693ce
sahand <sahand.vahidnia@yahoo.com>
2020-04-09 22:07:54 +1000

    few cleanups

--- n-gram corpus maker _ from preprocessed abstracts - by keywords.py
+++ n-gram corpus maker _ from preprocessed abstracts - by keywords.py
@@ -8,27 +8,16 @@ Created on Tue Apr  7 14:16:39 2020
 import pandas as pd
 import numpy as np
 from tqdm import tqdm
-import seaborn as sns
-
-import nltk
-from nltk.corpus import stopwords
-
 # =============================================================================
 # Init
 # =============================================================================
 print('\nInitializing...')
-sns.set_style('whitegrid')
-stops = ['a','an','we','result','however','yet','since','previously','although','propose','proposed','e_g','method',
-         'published_elsevier','b','v','problem','paper','approach','within','with','by','via','way','t','case','issue','level','area','system',
-         'work','discussed','seen','put','usually','take','make','author','versus','enables','result','research','design','based','al']
-nltk.download('stopwords')
-stop_words = list(set(stopwords.words("english")))+stops
 np.random.seed(50)
 
 root_path = '/home/sahand/GoogleDrive/Data/Corpus/copyr_lemmatized_stopword_removed_thesaurus/'
 data_abstracts = pd.read_csv(root_path+'1900-2019 abstract_title',names=['abstracts'])
 data_years = pd.read_csv(root_path+'1900-2019 years')
-data_keywords = pd.read_csv(root_path+'n-gram author and index keyword taxonomy.csv')
+data_keywords = pd.read_csv(root_path+'n-gram author keyword taxonomy.csv')
 
 wanted_grams = [2,3,4,5,6] # Statistically, 5 seems to be a proper cutting point as the frequency table suggests. Refer to : "Get statsitic of n in n-grams of corpus" block in drafts.
 periods = [[1990,2005],[2005,2008],[2008,2011],[2011,2014],[2014,2017],[2017,2019]]
@@ -51,7 +40,7 @@ for grams_count in tqdm(wanted_grams):
         data_keywords_tmp = data_keywords[(data_keywords.grams >= wanted_grams[idx])]
     else:
         data_keywords_tmp = data_keywords[(data_keywords.grams>=wanted_grams[idx]) & (data_keywords.grams<wanted_grams[idx+1])]
-    keywords_underscored = data_keywords_tmp.keywords.str.lower().str.strip().str.replace(' ','_').values.tolist()
+    keywords_underscored = data_keywords_tmp.keywords.str.lower().str.strip().str.replace(' ','_').str.upper().values.tolist()
     keywords_spaced = data_keywords_tmp.keywords.str.lower().str.strip().values.tolist()
     thesaurus.append(dict(zip(keywords_spaced,keywords_underscored)))
     idx+=1
