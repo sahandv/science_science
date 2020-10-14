92d56f83d4506fa7111858580412e9da90f70bce
Sahand <sahand.vahidnia@yahoo.com>
2020-10-14 17:17:00 +1100

    FoS parser added to DBLP parser

--- DBLP-json-parser.py
+++ DBLP-json-parser.py
@@ -22,23 +22,39 @@ input_file = '/mnt/6016589416586D52/Users/z5204044/Documents/Dataset//Aminer/dbl
 
 papers = []
 count = 0
+start = 0
 stop = 10000
 with open(input_file) as f:
     for line in f:
         count+=1
         if count > stop:
             break
-        try:
-            papers.append(json.loads(line))
-        except:
+        if count >= start:
             try:
-                line = line[1:]
                 papers.append(json.loads(line))
             except:
-                print('no way to parse the line ',count-1)
-                
+                try:
+                    line = line[1:] # remove the leading comma if exists
+                    papers.append(json.loads(line))
+                except:
+                    print('no way to parse the line ',count-1)
 
 papers = pd.DataFrame(papers)
+
+
+fos = papers['fos']
+fos_names = []
+for afos in fos:
+    try:
+        if len(afos)>0:
+            afos_names = [x['name'] for x in afos if pd.notnull(x)]
+            fos_names = fos_names+afos_names
+    except:
+        # print('Null FoS')
+        pass
+    
+fos_names = list(pd.DataFrame(fos_names,columns=['fos']).groupby('fos').groups.keys())
+
 papers['abstract'] = papers['indexed_abstract'].progress_apply(
     lambda x: ta.indexed_text_to_string(x['InvertedIndex'],x['IndexLength']) if pd.notnull(x) else np.nan)
 
