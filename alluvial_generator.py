536e0989b877ded927fc257336cb0652f6221678
Sahand <sahand.vahidnia@yahoo.com>
2020-11-03 18:17:31 +1100

    WIP: alluvial. minor change

--- alluvial_generator.py
+++ alluvial_generator.py
@@ -5,10 +5,29 @@ Created on Tue Nov  3 17:59:55 2020
 
 @author: sahand
 """
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
- 
+
+
+# =============================================================================
+# Data prep
+# =============================================================================
+path = '/mnt/6016589416586D52/Users/z5204044/GoogleDrive/GoogleDrive/Data/'
+labels = pd.read_csv(path+'Corpus/AI 4k/embeddings/clustering/k10/Doc2Vec patent_wos_ai corpus DEC 200,500,10 k10 labels')
+years = pd.read_csv(path+'Corpus/AI 4k/embeddings/clustering/k10/years 1990-2019.csv')
+df = labels.copy()
+df['year'] = years['Year']
+year_list = list(df.groupby('year').groups.keys())
+cluster_list = list(df.groupby('label').groups.keys())
+
+X = year_list
+
+df['year']
+Y = # list of lists
+
+
 # Create data
 X = np.arange(0, 10, 1)
 Y = X + 5 * np.random.random((5, X.size))
