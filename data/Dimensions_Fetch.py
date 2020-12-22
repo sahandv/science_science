feecb4da80c01ef1f1a05bdfeae0cebaadce167f
Sahand <sahand.vahidnia@yahoo.com>
2020-12-22 12:25:10 +1100

    Dimensions data downloader improvements

--- data/Dimensions_Fetch.py
+++ data/Dimensions_Fetch.py
@@ -9,21 +9,30 @@ This script is for fetching dimensions data
 """
 
 import dimcli
+import pandas as pd
 # dimcli.login(key="my-secret-key",endpoint="https://app.dimensions.ai")
 # dimcli --init
 dimcli.login()
 dsl = dimcli.Dsl()
 
+# =============================================================================
+# Query
+# =============================================================================
+
 data = dsl.query("""search publications for "black holes" return publications""")
 data = dsl.query("""search publications for "black holes" return publications""", verbose=False)
-data = dsl.query_iterative("""search publications for "black holes" where year=1990 and times_cited > 10 return publications""")
+data = dsl.query_iterative("""search publications for "artificial intelligence" where year=1990 and times_cited > 10 return publications 
+                           [id + authors + linkout + doi + title + abstract + reference_ids + year + category_bra + category_for]""")
 
+# =============================================================================
+# Process & Save
+# =============================================================================
 
 data.json.keys()
 len(data.publications)
 print("We got", data.count_batch, "results out of", data.count_total)
  # ps errors are printed out by default
-data = dsl.query("""search publications for "black holes" return spaceships""")
 print(data.errors_string)
 
-
+csv = pd.DataFrame(data.publications)
+csv.to_csv('~/dimensions AI 1990 cited>10.csv')
