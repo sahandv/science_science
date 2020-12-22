c976558a76c5a1738c81e8b9cf6a2a9aae43b221
Sahand <sahand.vahidnia@yahoo.com>
2020-12-22 17:42:21 +1100

    gcnn sample

--- Graph Embedding/graph_embedding_gcnn.py
+++ Graph Embedding/graph_embedding_gcnn.py
@@ -4,6 +4,8 @@
 Created on Tue Dec 15 10:44:23 2020
 
 @author: sahand
+@credits: https://towardsdatascience.com/understanding-graph-convolutional-networks-for-node-classification-a2bfdb7aba7b
+
 """
 
 import networkx as nx
@@ -57,6 +59,120 @@ print('\nAdjacency Matrix (A):\n', A)
 print('\nNode Features Matrix (X):\n', X)
 
 #Dot product Adjacency Matrix (A) and Node Features (X)
+# The dot product of Adjacency Matrix and Node Features Matrix represents the sum of neighboring node features.
+# AX sums up the adjacent node features, but it does not take into account the features of the node itself.
 AX = np.dot(A,X)
 print("Dot product of A and X (AX):\n", AX)
 
+# =============================================================================
+# Inserting Self-Loops
+# =============================================================================
+
+#Add Self Loops
+G_self_loops = G.copy()
+
+self_loops = []
+for i in range(G.number_of_nodes()):
+    self_loops.append((i,i))
+
+G_self_loops.add_edges_from(self_loops)
+
+#Check the edges of G_self_loops after adding the self loops
+print('Edges of G with self-loops:\n', G_self_loops.edges)
+
+#Get the Adjacency Matrix (A) and Node Features Matrix (X) of added self-lopps graph
+# A with self-loops added is A_hat
+A_hat = np.array(nx.attr_matrix(G_self_loops, node_attr='name')[0])
+print('Adjacency Matrix of added self-loops G (A_hat):\n', A_hat)
+
+#Calculate the dot product of A_hat and X (AX)
+AX = np.dot(A_hat, X)
+print('AX:\n', AX)
+
+# =============================================================================
+# GCN Normalization
+# 
+# Similar to data pre-processing for any Neural Networks operation, 
+#   we need to normalize the features to prevent numerical instabilities 
+#   and vanishing/exploding gradients
+# 
+# Calculating the Degree Matrix (D) and performing dot product operation of 
+#   the inverse of D with AX
+# =============================================================================
+
+
+#Get the Degree Matrix of the added self-loops graph
+Deg_Mat = G_self_loops.degree()
+print('Degree Matrix of added self-loops G (D): ', Deg_Mat)
+
+#Convert the Degree Matrix to a N x N matrix where N is the number of nodes
+D = np.diag([deg for (n,deg) in list(Deg_Mat)])
+print('Degree Matrix of added self-loops G as numpy array (D):\n', D)
+
+#Find the inverse of Degree Matrix (D)
+D_inv = np.linalg.inv(D)
+print('Inverse of D:\n', D_inv)
+
+#Dot product of D and AX for normalization
+DAX = np.dot(D_inv,AX)
+print('DAX:\n', DAX)
+
+#Symmetrically-normalization: https://arxiv.org/pdf/1609.02907.pdf
+D_half_norm = fractional_matrix_power(D, -0.5)
+DADX = D_half_norm.dot(A_hat).dot(D_half_norm).dot(X)
+print('DADX:\n', DADX)
+# =============================================================================
+# Adding Weights and Activation Function
+# =============================================================================
+#Initialize the weights
+np.random.seed(77777)
+n_h = 4 #number of neurons in the hidden layer
+n_y = 2 #number of neurons in the output layer
+W0 = np.random.randn(X.shape[1],n_h) * 0.01
+W1 = np.random.randn(n_h,n_y) * 0.01
+
+#Implement ReLu as activation function
+def relu(x):
+    return np.maximum(0,x)
+
+#Build GCN layer
+#In this function, we implement numpy to simplify
+def gcn(A,H,W):
+    I = np.identity(A.shape[0]) #create Identity Matrix of A
+    A_hat = A + I #add self-loop to A
+    D = np.diag(np.sum(A_hat, axis=0)) #create Degree Matrix of A
+    D_half_norm = fractional_matrix_power(D, -0.5) #calculate D to the power of -0.5
+    eq = D_half_norm.dot(A_hat).dot(D_half_norm).dot(H).dot(W)
+    return relu(eq)
+
+
+#Do forward propagation
+H1 = gcn(A,X,W0)
+H2 = gcn(A,H1,W1)
+print('Features Representation from GCN output:\n', H2)
+
+# =============================================================================
+# Visualize
+# =============================================================================
+def plot_features(H2):
+    #Plot the features representation
+    x = H2[:,0]
+    y = H2[:,1]
+
+    size = 1000
+
+    plt.scatter(x,y,size)
+    plt.xlim([np.min(x)*0.9, np.max(x)*1.1])
+    plt.ylim([-1, 1])
+    plt.xlabel('Feature Representation Dimension 0')
+    plt.ylabel('Feature Representation Dimension 1')
+    plt.title('Feature Representation')
+
+    for i,row in enumerate(H2):
+        str = "{}".format(i)
+        plt.annotate(str, (row[0],row[1]),fontsize=18, fontweight='bold')
+
+    plt.show()
+
+
+plot_features(H2)
