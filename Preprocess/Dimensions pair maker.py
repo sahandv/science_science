aa95d92290ef212a65ee2ecce3b9deb315c46efb
Sahand <sahand.vahidnia@yahoo.com>
2021-01-20 10:26:37 +1100

    WIP: co-citation maker

--- Preprocess/Dimensions pair maker.py
+++ Preprocess/Dimensions pair maker.py
@@ -13,13 +13,13 @@ from tqdm import tqdm
 # dir_root = '/mnt/6016589416586D52/Users/z5204044/GoogleDrive/GoogleDrive/Data/'
 dir_root = '/mnt/16A4A9BCA4A99EAD/GoogleDrive/Data/'
 
-data = pd.read_csv(dir_root+'Corpus/Dimensions/corpus references',names=['refs'])
-data_pub_ids = pd.read_csv(dir_root+'Corpus/Dimensions/publication idx',names=['pub_id'])
+data = pd.read_csv(dir_root+'Corpus/Dimensions - AI/corpus references',names=['refs'])
+data_pub_ids = pd.read_csv(dir_root+'Corpus/Dimensions - AI/publication idx',names=['pub_id'])
 data['pub_id'] = data_pub_ids['pub_id']
 data = data[pd.notnull(data['refs'])]
 
 # =============================================================================
-# Make pairs
+# Make citation pairs
 # =============================================================================
 pairs = []
 for idx,paper in tqdm(data.iterrows(),total=data.shape[0]):
@@ -37,3 +37,25 @@ pairs = pd.read_csv(dir_root+'Corpus/Dimensions/citations pairs')
 mask = data_pub_ids['pub_id'].values.tolist()
 pairs_masked = pairs[pairs['cited'].isin(mask)]
 pairs_masked.to_csv(dir_root+'Corpus/Dimensions/citations pairs - masked',index=False)
+
+# =============================================================================
+# Make co-citation pairs
+# =============================================================================
+# refs = data['refs'].apply(lambda x: x[1:-1].replace("'",'').split(', ')).values.tolist()
+# refs = [item for sublist in refs for item in sublist]
+# refs.extend(data['pub_id'].values.tolist())
+# refs = list( dict.fromkeys(refs) ) # remove duplicates
+
+pairs = []
+for idx,paper in tqdm(data.iterrows(),total=data.shape[0]):
+    refs = paper['refs'][1:-1].replace("'",'').split(', ')
+    for ref in refs:
+        pairs.append([paper['pub_id'],ref])
+        
+pairs = pd.DataFrame(pairs,columns=['citing','cited'])
+pair_groups = pairs.groupby('cited').groups
+pair_groups_cited = [list(x) for x in list(pair_groups.values()) if len(list(x))>1]
+
+# group = pair_groups_cited[0]
+# for i in group:
+    
