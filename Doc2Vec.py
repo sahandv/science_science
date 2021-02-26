f61b83dab35b56748f11f7a05b6c23e5f666a9f7
Sahand <sahand.vahidnia@yahoo.com>
2021-02-26 16:57:08 +1100

    minor changes

--- Doc2Vec.py
+++ Doc2Vec.py
@@ -14,25 +14,22 @@ from tqdm import tqdm
 
 # dir_root = '/mnt/6016589416586D52/Users/z5204044/GoogleDrive/GoogleDrive/Data/'
 dir_root = '/mnt/16A4A9BCA4A99EAD/GoogleDrive/Data/'
+texts = pd.read_csv(dir_root+'Corpus/cora-classify/cora/clean/with citations new/abstract_title all-lem',names=['abstract'])['abstract'].values.tolist()
 
 # =============================================================================
 # Train Model
 # =============================================================================
-texts = pd.read_csv(dir_root+'Corpus/Dimensions/abstract_title deflemm',names=['abstract'])['abstract'].values.tolist()
 documents = [TaggedDocument(doc.lower().split(), [i]) for i, doc in enumerate(texts)]
-model = Doc2Vec(documents, size=128, window=10, min_count=1, dm=1, workers=16, epochs=40)
-fname = dir_root+'Corpus/Dimensions/models/doc2vec 128D dm=1 window=10'
+model = Doc2Vec(documents, size=300, window=10, min_count=1, dm=1, workers=16, epochs=40)
+fname = dir_root+'Corpus/cora-classify/cora/models/doc2vec 300D dm=1 window=10'
 model.save(fname)
 
 
 # =============================================================================
 # Test Model
 # =============================================================================
-fname = dir_root+'Corpus/Dimensions/models/doc2vec 128D dm=1 window=10'
+fname = dir_root+'Corpus/cora-classify/cora/models/doc2vec 300D dm=1 window=10'
 model = Doc2Vec.load(fname)
-
-fname = dir_root+'Corpus/Dimensions/abstract_title deflemm'
-texts = pd.read_csv(fname,names=['abstract'])['abstract'].values.tolist()
 test_docs = [doc.lower().split() for doc in texts]
 # test_docs = test_docs[480000:]
 
@@ -43,4 +40,4 @@ X=[]
 for d in tqdm(test_docs):
     X.append( model.infer_vector(d, alpha=start_alpha, steps=infer_epoch))
 X_df = pd.DataFrame(X)
-X_df.to_csv(dir_root+'Corpus/Dimensions/embeddings/doc2vec 128D dm=1 window=10',index=False)
+X_df.to_csv(dir_root+'Corpus/cora-classify/cora/embeddings/doc2vec 300D dm=1 window=10',index=False)
--- clustering_benchmark_single-instance.py
+++ clustering_benchmark_single-instance.py
@@ -34,7 +34,7 @@ from DEC.DEC_keras import DEC_simple_run
 # =============================================================================
 datapath = '/mnt/16A4A9BCA4A99EAD/GoogleDrive/Data/'
 
-data_address =  datapath+"Corpus/cora-classify/cora/embeddings/node2vec super-d2v-node 128-80-10 p1q025"
+data_address =  datapath+"Corpus/cora-classify/cora/embeddings/doc2vec 300D dm=1 window=10"#node2vec super-d2v-node 128-70-20 p1q025"
 label_address = datapath+"Corpus/cora-classify/cora/clean/with citations new/corpus classes1"
 
 # data_address =  datapath+"Corpus/KPRIS/embeddings/deflemm/Doc2Vec patent_wos corpus"
@@ -48,14 +48,6 @@ labels_f = pd.factorize(labels.label)
 X = vectors.values
 Y = labels_f[0]
 n_clusters = len(list(labels.groupby('label').groups.keys())) 
-
-
-labels_task_1 = labels[(labels['label']=='car') | (labels['label']=='memory')]
-vectors_task_1 = vectors.iloc[labels_task_1.index]
-labels_task_1_f = pd.factorize(labels_task_1.label)
-X_task_1 = vectors_task_1.values
-Y_task_1 = labels_task_1_f[0]
-n_clusters_task_1 = 2
 
 results = pd.DataFrame([],columns=['Method','parameter','Silhouette','Homogeneity','NMI','AMI','ARI'])
 # =============================================================================
@@ -92,7 +84,7 @@ results = results.append(tmp_results, ignore_index=True)
 # K-means
 # =============================================================================
 print('\n- k-means random -----------------------')
-for fold in tqdm(range(20)):
+for fold in tqdm(range(10)):
     seed = randint(0,10**5)
     model = KMeans(n_clusters=n_clusters,n_init=20, init='random', random_state=seed).fit(X)
     predicted_labels = model.labels_
@@ -106,23 +98,23 @@ print(maxx)
 # =============================================================================
 # K-means with init='k-means++'
 # =============================================================================
-print('\n- k-means++ -----------------------')
-for fold in tqdm(range(20)):
-    seed = randint(0,10**5)
-    model = KMeans(n_clusters=n_clusters,n_init=20,init='k-means++', random_state=seed).fit(X)
-    predicted_labels = model.labels_
-    tmp_results = ['k-means++','seed '+str(seed)]+evaluate(X,Y,predicted_labels)
-    tmp_results = pd.Series(tmp_results, index = results.columns)
-    results = results.append(tmp_results, ignore_index=True)
-mean = results.mean(axis=0)
-maxx = results.max(axis=0)
-print(mean)
-print(maxx)
+# print('\n- k-means++ -----------------------')
+# for fold in tqdm(range(20)):
+#     seed = randint(0,10**5)
+#     model = KMeans(n_clusters=n_clusters,n_init=20,init='k-means++', random_state=seed).fit(X)
+#     predicted_labels = model.labels_
+#     tmp_results = ['k-means++','seed '+str(seed)]+evaluate(X,Y,predicted_labels)
+#     tmp_results = pd.Series(tmp_results, index = results.columns)
+#     results = results.append(tmp_results, ignore_index=True)
+# mean = results.mean(axis=0)
+# maxx = results.max(axis=0)
+# print(mean)
+# print(maxx)
 # =============================================================================
 # Agglomerative
 # =============================================================================
 print('\n- Agglomerative -----------------------')
-for fold in tqdm(range(4)):
+for fold in tqdm(range(1)):
     model = AgglomerativeClustering(n_clusters=n_clusters,linkage='ward').fit(X)
     predicted_labels = model.labels_
     tmp_results = ['Agglomerative','ward']+evaluate(X,Y,predicted_labels)
@@ -135,37 +127,37 @@ print(maxx)
 # =============================================================================
 # DBSCAN
 # =============================================================================
-eps=0.000001
-print('\n- DBSCAN -----------------------')
-for fold in tqdm(range(19)):
-    eps = eps+0.05
-    model = DBSCAN(eps=eps, min_samples=10,n_jobs=15).fit(X)
-    predicted_labels = model.labels_
-    tmp_results = ['DBSCAN','eps '+str(eps)]+evaluate(X,Y,predicted_labels)
-    tmp_results = pd.Series(tmp_results, index = results.columns)
-    results = results.append(tmp_results, ignore_index=True)
-mean = results.mean(axis=0)
-maxx = results.max(axis=0)
-print(mean)
-print(maxx)
+# eps=0.000001
+# print('\n- DBSCAN -----------------------')
+# for fold in tqdm(range(19)):
+#     eps = eps+0.05
+#     model = DBSCAN(eps=eps, min_samples=10,n_jobs=15).fit(X)
+#     predicted_labels = model.labels_
+#     tmp_results = ['DBSCAN','eps '+str(eps)]+evaluate(X,Y,predicted_labels)
+#     tmp_results = pd.Series(tmp_results, index = results.columns)
+#     results = results.append(tmp_results, ignore_index=True)
+# mean = results.mean(axis=0)
+# maxx = results.max(axis=0)
+# print(mean)
+# print(maxx)
 # =============================================================================
 # Deep no min_max_scaling
 # =============================================================================
 archs = [[500, 500, 2000, 10],[500, 1000, 2000, 10],[500, 1000, 1000, 10],
-         [500, 500, 2000, 100],[500, 1000, 2000, 100],[500, 1000, 1000, 100],
-         [100, 300, 600, 10],[300, 500, 2000, 10],[700, 1000, 2000, 10],
+         # [500, 500, 2000, 100],[500, 1000, 2000, 100],[500, 1000, 1000, 100],
+         # [100, 300, 600, 10],[300, 500, 2000, 10],[700, 1000, 2000, 10],
          [200, 500, 10],[500, 1000, 10],[1000, 2000, 10],
          [200, 500, 100],[500, 1000, 100],[1000, 2000, 100],
-         [1000, 500, 10],[500, 200, 10],[200, 100, 10],
-         [1000, 1000, 2000, 10],[1000, 1500, 2000, 10],[1000, 1500, 1000, 10],
-         [1000, 1000, 2000,500, 10],[1000, 1500, 2000,500, 10],[1000, 1500, 1000, 500, 10],
+         # [1000, 500, 10],[500, 200, 10],[200, 100, 10],
+         # [1000, 1000, 2000, 10],[1000, 1500, 2000, 10],[1000, 1500, 1000, 10],
+         # [1000, 1000, 2000,500, 10],[1000, 1500, 2000,500, 10],[1000, 1500, 1000, 500, 10],
          [500, 500, 2000, 500, 10],[500, 1000, 2000, 500, 10],[500, 1000, 1000, 500, 10]]
 
 print('\n- DEC -----------------------')
 for fold in tqdm(archs):
     seed = randint(0,10**4)
     np.random.seed(seed)
-    predicted_labels = DEC_simple_run(X,minmax_scale_custom_data=False,n_clusters=5,architecture=fold,pretrain_epochs=300)
+    predicted_labels = DEC_simple_run(X,minmax_scale_custom_data=False,n_clusters=n_clusters,architecture=fold,pretrain_epochs=300)
     tmp_results = ['DEC',str(seed)+' '+str(fold)]+evaluate(X,Y,predicted_labels)
     tmp_results = pd.Series(tmp_results, index = results.columns)
     results = results.append(tmp_results, ignore_index=True)
@@ -176,34 +168,37 @@ print(maxx)
 # =============================================================================
 # Deep with min_max_scaling
 # =============================================================================
-archs = [[500, 500, 2000, 10],[500, 1000, 2000, 10],[500, 1000, 1000, 10],
-         [500, 500, 2000, 100],[500, 1000, 2000, 100],[500, 1000, 1000, 100],
-         [100, 300, 600, 10],[300, 500, 2000, 10],[700, 1000, 2000, 10],
-         [200, 500, 10],[500, 1000, 10],[1000, 2000, 10],
-         [200, 500, 100],[500, 1000, 100],[1000, 2000, 100],
-         [1000, 500, 10],[500, 200, 10],[200, 100, 10],
-         [1000, 1000, 2000, 10],[1000, 1500, 2000, 10],[1000, 1500, 1000, 10],
-         [1000, 1000, 2000,500, 10],[1000, 1500, 2000,500, 10],[1000, 1500, 1000, 500, 10],
-         [500, 500, 2000, 500, 10],[500, 1000, 2000, 500, 10],[500, 1000, 1000, 500, 10]]
-print('\n- DEC -----------------------')
-for fold in tqdm(archs):
-    seed = randint(0,10**4)
-    np.random.seed(seed)
-    predicted_labels = DEC_simple_run(X,minmax_scale_custom_data=False,n_clusters=5,architecture=fold,pretrain_epochs=300)
-    tmp_results = ['DEC',str(seed)+' '+str(fold)]+evaluate(X,Y,predicted_labels)
-    tmp_results = pd.Series(tmp_results, index = results.columns)
-    results = results.append(tmp_results, ignore_index=True)
-mean = results.mean(axis=0)
-maxx = results.max(axis=0)
-print(mean)
-print(maxx)
+# archs = [[500, 500, 2000, 10],[500, 1000, 2000, 10],[500, 1000, 1000, 10],
+#          [500, 500, 2000, 100],[500, 1000, 2000, 100],[500, 1000, 1000, 100],
+#          [100, 300, 600, 10],[300, 500, 2000, 10],[700, 1000, 2000, 10],
+#          [200, 500, 10],[500, 1000, 10],[1000, 2000, 10],
+#          [200, 500, 100],[500, 1000, 100],[1000, 2000, 100],
+#          [1000, 500, 10],[500, 200, 10],[200, 100, 10],
+#          [1000, 1000, 2000, 10],[1000, 1500, 2000, 10],[1000, 1500, 1000, 10],
+#          [1000, 1000, 2000,500, 10],[1000, 1500, 2000,500, 10],[1000, 1500, 1000, 500, 10],
+#          [500, 500, 2000, 500, 10],[500, 1000, 2000, 500, 10],[500, 1000, 1000, 500, 10]]
+# print('\n- DEC -----------------------')
+# for fold in tqdm(archs):
+#     seed = randint(0,10**4)
+#     np.random.seed(seed)
+#     predicted_labels = DEC_simple_run(X,minmax_scale_custom_data=False,n_clusters=5,architecture=fold,pretrain_epochs=300)
+#     tmp_results = ['DEC',str(seed)+' '+str(fold)]+evaluate(X,Y,predicted_labels)
+#     tmp_results = pd.Series(tmp_results, index = results.columns)
+#     results = results.append(tmp_results, ignore_index=True)
+# mean = results.mean(axis=0)
+# maxx = results.max(axis=0)
+# print(mean)
+# print(maxx)
 # =============================================================================
 # Just cluster
 # =============================================================================
 
-print('\n- k-means++ -----------------------')
-seed = randint(0,10**5)
+print('\n- k-means -----------------------')
+seed = 80063
 model = KMeans(n_clusters=n_clusters,n_init=20,init='k-means++', random_state=seed).fit(X)
+predicted_labels = model.labels_
+
+model = KMeans(n_clusters=n_clusters,n_init=20, init='random', random_state=seed).fit(X)
 predicted_labels = model.labels_
 
 results_df = pd.DataFrame(predicted_labels,columns=['label'])
@@ -240,4 +235,4 @@ for cluster in tqdm(range(n_clusters),total=n_clusters):
 # Save clusters
 # =============================================================================
 predicted_labels = pd.DataFrame(predicted_labels,columns=['labels'])
-predicted_labels.to_csv(datapath+"Corpus/cora-classify/cora/embeddings/doc2vec all-lem 128D dm=1 window=10 predictions",index=False)
+predicted_labels.to_csv(datapath+"Corpus/cora-classify/cora/embeddings/doc2vec all-lem 300D dm=1 window=10 predictions",index=False)
