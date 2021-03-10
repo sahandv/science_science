1f2135b0e1f6019e009cd8fdb552da8980116c14
Sahand <sahand.vahidnia@yahoo.com>
2021-03-10 13:49:40 +1100

    added component filter

--- Preprocess/Cora data preprocess.py
+++ Preprocess/Cora data preprocess.py
@@ -22,13 +22,16 @@ This is a preprocessing script for Cora dataset [McCallumIRJ]
 # =============================================================================
 # Init
 # =============================================================================
-dir_path = '/mnt/16A4A9BCA4A99EAD/GoogleDrive/Data/Corpus/cora-classify/cora/'
+# dir_path = '/mnt/16A4A9BCA4A99EAD/GoogleDrive/Data/Corpus/cora-classify/cora/'      # ryzen
+dir_path = '/mnt/6016589416586D52/Users/z5204044/GoogleDrive/GoogleDrive/Data/Corpus/cora-classify/cora/'      # c1314
+
 
 import json   
 from os import listdir
 from os.path import isfile, join
 import pandas as pd
 import numpy as np
+import networkx as nx
 from tqdm import tqdm
 import gc
 tqdm.pandas()
@@ -135,6 +138,7 @@ data = pd.read_csv(dir_path+'extractions_with_id.csv')
 data_clean = data[pd.notna(data['Abstract'])]
 data_clean = data_clean[data_clean['Abstract']!='']
 data_clean = data_clean[data_clean['Abstract']!=' ']
+data_clean = data_clean[pd.notnull(data_clean['Abstract'])]
 data_clean = data_clean[pd.notna(data_clean['Title'])]
 data_clean = data_clean[pd.notna(data_clean['id'])]
 data_clean_unique = data_clean.groupby('id').first().reset_index()
@@ -148,9 +152,30 @@ data = pd.read_csv(dir_path+'extractions_with_unique_id_labeled.csv')
 sample = data.sample(500)
 id_list = data['id'].values.tolist()
 
-filtered_citations = citations[((citations['referring_id'].isin(id_list)) | (citations['cited_id'].isin(id_list)))]
+filtered_citations = citations[(citations['referring_id'].isin(id_list)) | (citations['cited_id'].isin(id_list))]
 filtered_citations.to_csv(dir_path+'citations_filtered.csv',index=False)
 
+# =============================================================================
+# Filter and take the largest component
+# =============================================================================
 
+# dir_path = '/mnt/16A4A9BCA4A99EAD/GoogleDrive/Data/Corpus/cora-classify/cora/'
+data = pd.read_csv(dir_path+'extractions_with_unique_id_labeled.csv')
+citations = pd.read_csv(dir_path+'citations_filtered.csv')# with_d2v300D_supernodes.csv')#, names=['referring_id','cited_id'],sep='\t')
+citations.columns = ['referring_id','cited_id']
+
+citations.info(memory_usage='deep')
+
+graph = nx.Graph()
+for i,row in tqdm(citations.iterrows(),total=citations.shape[0]):
+    graph.add_edge(row['referring_id'],(row['cited_id']))
+    
+
+print('Graph fully connected:',nx.is_connected(graph))
+print('Connected components:',nx.number_connected_components(graph))
+giant_connected_component = list(max(nx.connected_components(graph), key=len))
+
+data_filtered = data[data['id'].isin(giant_connected_component)]
+data_filtered.to_csv(dir_path+'extractions_with_unique_id_labeled_single_component.csv',index=False)
 
 
