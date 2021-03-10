a30737a7ecf893629016e93ea84b7824d0dcd888
Sahand <sahand.vahidnia@yahoo.com>
2021-03-11 00:53:13 +1100

    baseline methods on karateclub updated.
    Complete waste of time and useless. Too many conditions to satisfy.

--- Graph Embedding/graph_embedding_baselines.py
+++ Graph Embedding/graph_embedding_baselines.py
@@ -12,31 +12,43 @@ import numpy as np
 import networkx as nx
 import karateclub as kc
 from tqdm import tqdm
-from sklearn.feature_extraction.text import TfidfTransformer, TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer, TfidfVectorizer,CountVectorizer
 
 # =============================================================================
 # Init Cora
 # =============================================================================
 dir_root = '/mnt/16A4A9BCA4A99EAD/GoogleDrive/Data/Corpus/cora-classify/cora/'
-texts = pd.read_csv(dir_root+'clean/with citations new/abstract_title all-lem',names=['abstract'])['abstract'].values.tolist()
-networks = pd.read_csv(dir_root+'citations_filtered.csv')# with_d2v300D_supernodes.csv')#, names=['referring_id','cited_id'],sep='\t')
+texts = pd.read_csv(dir_root+'clean/single_component/abstract_title all-lem',names=['abstract'])['abstract'].values.tolist()
+networks = pd.read_csv(dir_root+'citations_filtered_single_component.csv')# with_d2v300D_supernodes.csv')#, names=['referring_id','cited_id'],sep='\t')
 networks.columns = ['referring_id','cited_id']
 
 gc.collect()
 sample = networks.sample()
 networks.info(memory_usage='deep')
-idx = pd.read_csv(dir_root+'clean/with citations new/corpus idx')#,names=['id'])#(dir_path+'corpus idx',index_col=0)
+idx = pd.read_csv(dir_root+'clean/single_component/corpus idx')#,names=['id'])#(dir_path+'corpus idx',index_col=0)
 idx.columns = ['id']
 # idx['id'] = idx['id'].str.replace('pub.','').astype(str).astype(int)
 idx = idx['id'].values.tolist()
+networks = networks[(networks['referring_id'].isin(idx)) | (networks['cited_id'].isin(idx))] # mask
+
+# =============================================================================
+# translate  node_indices to numeric_indices
+# =============================================================================
+nodes = list(set(networks['referring_id'].values.tolist()+networks['cited_id'].values.tolist()))
+
+dictionary = dict()
+for i,value in tqdm(enumerate(idx)):
+    dictionary[value]=i
+
+networks_new = networks.replace(dictionary)
+
+new_nodes = list(set(networks_new['referring_id'].values.tolist()+networks_new['cited_id'].values.tolist()))
 
 # =============================================================================
 # Prepare graph
 # =============================================================================
-networks = networks[(networks['referring_id'].isin(idx)) | (networks['cited_id'].isin(idx))] # mask
-
 graph = nx.Graph()
-for i,row in tqdm(networks.iterrows(),total=networks.shape[0]):
+for i,row in tqdm(networks_new.iterrows(),total=networks_new.shape[0]):
     graph.add_edge(row['referring_id'],row['cited_id'])
 
 print('Graph fully connected:',nx.is_connected(graph))
@@ -44,8 +56,14 @@ print('Connected components:',nx.number_connected_components(graph))
 
 # connected_components = list(nx.connected_components(graph))
 
-del networks
+# del networks
 gc.collect()
+# =============================================================================
+# Text embedding (BoW)
+# =============================================================================
+count_vect = CountVectorizer()
+X = count_vect.fit_transform(texts)
+X.shape
 # =============================================================================
 # Text embedding (TFIDF)
 # =============================================================================
@@ -56,12 +74,17 @@ X.shape
 # TADW
 # =============================================================================
 TADW = kc.node_embedding.TADW(dimensions=120, reduction_dimensions=240,lambd=1)
-model_TADW = TADW.fit(graph,X)
-TADW_vectors = model_TADW.get_embedding() 
+TADW.fit(graph,X)
+TADW_vectors = TADW.get_embedding() 
 # =============================================================================
 # TENE
 # =============================================================================
 TENE = kc.node_embedding.TENE()
-model_TENE = TENE.fit(graph,X)
-TENE_vectors = model_TENE.get_embedding() 
-
+TENE.fit(graph,X)
+TENE_vectors = TENE.get_embedding() 
+# =============================================================================
+# DeepWalk
+# =============================================================================
+DW = kc.DeepWalk()
+DW.fit(graph)
+DW_vectors = DW.get_embedding() 
