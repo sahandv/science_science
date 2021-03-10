27eeb455a1ab3c3de551838f3b79815d53ee87b5
Sahand <sahand.vahidnia@yahoo.com>
2021-03-10 17:52:42 +1100

    WIP merging

--- Graph Embedding/graph_embedding_nonlineear_merge.py
+++ Graph Embedding/graph_embedding_nonlineear_merge.py
@@ -4,5 +4,34 @@
 Created on Tue Mar  9 10:50:38 2021
 
 @author: github.com/sahandv
+
+take ideas from:
+    https://towardsdatascience.com/multi-class-text-classification-with-lstm-1590bee1bd17
+    https://github.com/susanli2016/NLP-with-Python/blob/master/Multi-Class%20Text%20Classification%20LSTM%20Consumer%20complaints.ipynb
+    https://machinelearningmastery.com/use-word-embedding-layers-deep-learning-keras/
 """
 
+import sys
+import gc
+import pandas as pd
+import numpy as np
+import networkx as nx
+import karateclub as kc
+from tqdm import tqdm
+from sklearn.feature_extraction.text import TfidfTransformer, TfidfVectorizer
+import tensorflow as tf
+from tensorflow.contrib.keras import layers
+
+# read labels
+
+# read doc vecs
+
+# read graph vecs
+
+# split data based on the labels and make them more balanced
+
+# split data for train/validation/test
+
+# train
+
+# get output 
