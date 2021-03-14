992ad04493df29ceeb481c4d5937cc0c7933db8f
Sahand <sahand.vahidnia@yahoo.com>
2021-03-15 01:56:31 +1100

    merging ready

--- Graph Embedding/graph_embedding_nonlineear_merge.py
+++ Graph Embedding/graph_embedding_nonlineear_merge.py
@@ -32,10 +32,18 @@ from keras.models import Sequential, load_model
 from keras.optimizers import SGD, Adam
 from keras.callbacks import ModelCheckpoint, EarlyStopping
 from keras.utils.np_utils import to_categorical# from tensorflow.contrib.keras import layers
+from keras.utils.vis_utils import plot_model
+from tensorflow.keras.layers import BatchNormalization
+
+from keras_visualizer import visualizer 
+from tensorflow.keras.callbacks import TensorBoard
+
+pretrain = True
+get_output = True
 
 # read labels
-# datapath = '/mnt/16A4A9BCA4A99EAD/GoogleDrive/Data/'
-datapath = '/home/sahand/GoogleDrive/Data/'
+datapath = '/mnt/16A4A9BCA4A99EAD/GoogleDrive/Data/'
+# datapath = '/home/sahand/GoogleDrive/Data/'
 data_dir =  datapath+'Corpus/cora-classify/cora/'
 label_address =  data_dir+'clean/single_component_small/labels'
 labels = pd.read_csv(label_address)
@@ -57,44 +65,65 @@ Y = pd.get_dummies(labels).values
 X = vec.values
 # make them more balanced
 
-
-# split data for train and future test
-Xpretrain, Xtrain, Ypretrain, Ytrain = train_test_split(X, Y, test_size=0.6, random_state=100,shuffle=True)
-Xpretrain_train, Xpretrain_test, Ypretrain_train, Ypretrain_test = train_test_split(X, Y, test_size=0.3, random_state=100,shuffle=True)
-
-
-# define model
-model_shape=[800,300,100]
-input_shape = 600
-act='relu'
-act_last='softmax'
-loss_f='categorical_crossentropy'
-opt='adam'
-init='glorot_uniform'
-
-x = Input(shape=(input_shape,), name='input')
-x_1 = x
-for i,dim in enumerate(model_shape[:-1]):
-    x_1 = Dense(dim, activation=act, name='hidden_'+str(i))(x_1)
-
-x_2 = Dense(model_shape[-1], activation=act, name='hidden_last')(x_1)
-# x_3 = Dense(model_shape[3], activation=act, kernel_initializer=init, name='hidden_3')(x_2)
-
-x_2 = concatenate([x_1, x_2])
-y = Dense(num_classes, activation=act_last, name='output')(x_2)#(combined)
-model = Model(inputs=x, outputs=y, name='classifier')
-model.compile(loss=loss_f, optimizer=opt, metrics=['accuracy'])
-model.summary()
-
-callback = EarlyStopping(monitor='val_loss',patience=80)
-checkpoint = ModelCheckpoint('models/pretrain_best_model.h5', monitor='val_loss', mode='min', save_best_only=True)
-
-print('classes',dict(pd.DataFrame(pd.DataFrame(Ypretrain_train).idxmax(axis=1),columns=['label']).groupby('label').groups).keys())
-# Xpretrain_test.shape
-# train
-history = model.fit(Xpretrain_train, Ypretrain_train, validation_data=(Xpretrain_test, Ypretrain_test), epochs=400, batch_size = 32, verbose=1, callbacks=[callback, checkpoint])
+Xtrain, Xtest, Ytrain, Ytest = train_test_split(X, Y, test_size=0.5, random_state=100,shuffle=True)
+if pretrain:
+    # split data for train and future test
+    Xpretrain_train, Xpretrain_test, Ypretrain_train, Ypretrain_test = train_test_split(Xtrain, Ytrain, test_size=0.3, random_state=100,shuffle=True)
+    
+    
+    # define model
+    model_shape=[800,600,200,100]
+    input_shape = 600
+    act='relu'
+    act_last='softmax'
+    loss_f='categorical_crossentropy'
+    opt='adam'
+    init='glorot_uniform'
+    
+    x = Input(shape=(input_shape,), name='input')
+    x_1 = x
+    x_1 = BatchNormalization(name='b_normalization_'+str(0))(x_1)
+    for i,dim in enumerate(model_shape[:-1]):
+        x_1 = Dense(dim, activation=act, name='hidden_'+str(i))(x_1)        
+    
+    x_2 = Dense(model_shape[-1], activation=act, name='hidden_'+str(i+1))(x_1)
+    # x_3 = Dense(model_shape[3], activation=act, kernel_initializer=init, name='hidden_3')(x_2)
+    
+    x_2 = concatenate([x_1, x_2], name='embedding')
+    
+    y = Dense(num_classes, activation=act_last, name='output')(x_2)
+    model = Model(inputs=x, outputs=y, name='classifier')
+    model.compile(loss=loss_f, optimizer=opt, metrics=['accuracy'])
+    model.summary()
+    
+    callback = EarlyStopping(monitor='val_accuracy',patience=35)
+    checkpoint = ModelCheckpoint('models/pretrain_best_model.h5', monitor='val_accuracy', mode='min', save_best_only=True)
+    tensorboard = TensorBoard(
+                              log_dir='.\logs',
+                              histogram_freq=1,
+                              write_images=True
+                            )
+    print('classes',dict(pd.DataFrame(pd.DataFrame(Ypretrain_train).idxmax(axis=1),columns=['label']).groupby('label').groups).keys())
+    # Xpretrain_test.shape
+    # train
+    history = model.fit(Xpretrain_train, Ypretrain_train, validation_data=(Xpretrain_test, Ypretrain_test), epochs=400, batch_size = 64, verbose=1, callbacks=[callback, checkpoint,tensorboard])
 
 # get output 
-# best_model = load_model('models/pretrain_best_model.h5')
-
-
+if get_output:
+    input_arr = X
+    
+    best_model = load_model('models/pretrain_best_model.h5')
+    plot_model(best_model, to_file='models/model_plot.png', show_shapes=True, show_layer_names=True)
+    
+    # get_output = K.function([best_model.input],[best_model.layers[5].output])
+    # output = get_output([np.expand_dims(input_arr,axis=0)])[0]
+    # output = get_output([input_arr])[0]
+    
+    # layer_name = 'embedding'
+    layer_name = 'hidden_2'
+    intermediate_layer_model = Model(inputs=best_model.input,outputs=best_model.get_layer(layer_name).output)
+    embeddings = pd.DataFrame(intermediate_layer_model.predict(X))
+    embeddings.to_csv(datapath+'Corpus/cora-classify/cora/embeddings/single_component_small/deep_nonlinear_embedding_600',index=False)
+    
+    
+    
