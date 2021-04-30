6d3b0905a71f35610000f0b104de31f44f94fec9
Sahand <sahand.vahidnia@yahoo.com>
2021-04-30 12:29:42 +1000

    added another mask

--- Graph Embedding/super_node_generator.py
+++ Graph Embedding/super_node_generator.py
@@ -8,13 +8,22 @@ Created on Mon Feb 22 12:30:13 2021
 
 import pandas as pd
 datapath = '/mnt/16A4A9BCA4A99EAD/GoogleDrive/Data/'
-idx_address = datapath+"Corpus/cora-classify/cora/clean/single_component_small/node_idx_seq"
-cluster_address = datapath+"Corpus/cora-classify/cora/embeddings/single_component_small/doc2vec 300D dm=1 window=10 clustering predictions"
-citations_address = datapath+'Corpus/cora-classify/cora/clean/single_component_small/network'
+idx_address = datapath+"Corpus/cora-classify/cora/clean/single_component_small_18k/node_idx_seq cocite"
+cluster_address = datapath+"Corpus/cora-classify/cora/embeddings/single_component_small_18k/doc2vec 300D dm=1 window=10 clustering predictions"
+citations_address = datapath+'Corpus/cora-classify/cora/citations'
+citations_filter_address =  datapath+"Corpus/cora-classify/cora/clean/single_component_small_18k/corpus_idx_original"
+
 
 idx = pd.read_csv(idx_address)
 clusters = pd.read_csv(cluster_address)
-citation_pairs = pd.read_csv(citations_address).values.tolist()
+citation_pairs = pd.read_csv(citations_address,sep='\t',names=['a','b'])
+
+#filter -- optional
+citations_filter =  pd.read_csv(citations_filter_address).values[:,0].tolist()
+citation_pairs = citation_pairs[(citation_pairs['a'].isin(citations_filter)) | (citation_pairs['b'].isin(citations_filter))] # mask
+
+
+citation_pairs = citation_pairs.values.tolist()
 
 idx_clusters = idx.copy()
 idx_clusters['cluster'] = clusters['labels']
@@ -30,5 +39,5 @@ for cluster in cluster_ids:
     citation_pairs = citation_pairs+cluster_papers
     
 
-citation_pairs = pd.DataFrame(citation_pairs,columns=['referring_id','cited_id'])
-citation_pairs.to_csv(datapath+'Corpus/cora-classify/cora/clean/single_component_small/network with_d2v300D_supernodes',index=False)
+citation_pairs = pd.DataFrame(citation_pairs,columns=['referring_id','cited_id'])#,'count'])
+citation_pairs.to_csv(datapath+'Corpus/cora-classify/cora/clean/single_component_small_18k/network_cocitation with_d2v300D_supernodes',index=False)
