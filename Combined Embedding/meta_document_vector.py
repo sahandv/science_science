c6d0852d87de7a19cd89e15ee95a23f4e1706862
Sahand <sahand.vahidnia@yahoo.com>
2021-06-16 13:50:51 +1000

    improved performance (epoch time 9h>5h)

--- Combined Embedding/meta_document_vector.py
+++ Combined Embedding/meta_document_vector.py
@@ -24,9 +24,15 @@ from tensorflow.keras import mixed_precision
 
 tokenizer = 'word'
 
+
 # =============================================================================
 # Prepare GPU
 # =============================================================================
+from numba import cuda 
+device = cuda.get_current_device()
+device.reset()
+
+
 os.environ['TF_ENABLE_AUTO_MIXED_PRECISION'] = '1'
 mixed_precision.set_global_policy('mixed_float16')
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
@@ -76,6 +82,7 @@ embedding_dim = 128
 num_epochs = 500
 vocab_limit = 50000
 n_docs = len(corpus_idx)+1
+min_paragraph_len = 50  #percentage of each paragraph
 
 if tokenizer=='word':
 ##################
@@ -90,7 +97,7 @@ if tokenizer=='word':
     # print(tokenizer.word_index)
     print(tokenizer.word_index['documentembeddingtoken'])
     
-    n = 25
+    
     ##################
     # unlimited length
     ##################
@@ -99,7 +106,7 @@ if tokenizer=='word':
     input_sequences_doc_id = []
     for cid,sent in tqdm(enumerate(corpus),total=len(corpus)):
         token_list = tokenizer.texts_to_sequences([sent])[0]
-        for i in range(n,len(token_list)):
+        for i in range(int(len(token_list)*min_paragraph_len/100)-1,len(token_list)):
             n_gram_sentence = token_list[:i+1]
             input_sequences.append(n_gram_sentence)   
             input_sequences_doc_id.append(cid)
@@ -111,7 +118,7 @@ if tokenizer=='word':
     input_sequences_doc_id = []
     for cid,sent in tqdm(enumerate(corpus),total=len(corpus)):
         token_list = tokenizer.texts_to_sequences([sent])[0]
-        for i in range(n,min(len(token_list),max_paragraph_len)):
+        for i in range(int(len(token_list)*min_paragraph_len/100)-1,min(len(token_list),max_paragraph_len)):
             n_gram_sentence = token_list[:i+1]
             input_sequences.append(n_gram_sentence)
             input_sequences_doc_id.append(cid)
@@ -220,7 +227,7 @@ class DataGenerator(tf.keras.utils.Sequence):
     Generates data for Keras
     Sequence based data generator.
     """
-    def __init__(self,x1,x2,y=None,corpus_idx=None,n_classes:int=None,n_docs:int=None,batch_size=5,to_fit=True):
+    def __init__(self,x1,x2,y=None,corpus_idx=None,n_classes:int=None,n_docs:int=None,batch_size=5,to_fit=True,n_inputs=3):
         self.x1 = x1
         self.x2 = x2
         self.y = y
@@ -230,12 +237,42 @@ class DataGenerator(tf.keras.utils.Sequence):
         self.batch_size = batch_size
         self.to_fit = to_fit
         self.ids = list(range(len(x1)))
+        self.n_inputs = n_inputs
         self.IDmemory = None
         self.Imemory = None
         
     def __len__(self):
         return int(np.floor(self.x1.shape[0] / self.batch_size))
     
+    # def __getitem__(self, index):
+    #     """
+    #     Generate one batch of data
+        
+    #     Parameters
+    #     -------
+    #     index: index of the batch
+        
+    #     Returns
+    #     -------
+    #     X and y when fitting. X only when predicting
+    #     W
+    #     Exptected network inputs:
+    #        [ inputs_seq , inputs_doc , inputs_netvec ]
+        
+    #     """
+    #     ID_list = self.ids[index * self.batch_size:(index + 1) * self.batch_size]
+    #     self.Imemory = index
+        
+    #     input_1 = self._generate_input_1(ID_list)
+    #     input_2 = self._generate_input_2(ID_list)
+    #     input_3 = self._generate_input_3(ID_list)
+        
+    #     if self.to_fit:
+    #         y = self._generate_y(ID_list)
+    #         return [input_1,input_2,input_3], y
+    #     else:
+    #         return [input_1,input_2,input_3]
+
     def __getitem__(self, index):
         """
         Generate one batch of data
@@ -255,16 +292,12 @@ class DataGenerator(tf.keras.utils.Sequence):
         ID_list = self.ids[index * self.batch_size:(index + 1) * self.batch_size]
         self.Imemory = index
         
-        input_1 = self._generate_input_1(ID_list)
-        input_2 = self._generate_input_2(ID_list)
-        input_3 = self._generate_input_3(ID_list)
-        
-        if self.to_fit:
-            y = self._generate_y(ID_list)
-            return [input_1,input_2,input_3], y
-        else:
-            return [input_1,input_2,input_3]
-        
+        inputs_1,inputs_2,inputs_3 = self._generate_input_123(ID_list)
+
+        y = self._generate_y(ID_list)
+        return [inputs_1,inputs_2,inputs_3], y
+
+    
     def _generate_y(self, ID_list):
         y_batch = np.empty((self.batch_size), dtype=int)
         for n,i in enumerate(ID_list):
@@ -285,7 +318,7 @@ class DataGenerator(tf.keras.utils.Sequence):
         x_batch = np.empty((self.batch_size), dtype=int)
         for n,i in enumerate(ID_list):
             x_batch[n] = self.corpus_idx[i]
-        return tf.keras.utils.to_categorical(x_batch, num_classes=self.n_docs) 
+        return(tf.keras.utils.to_categorical(x_batch, num_classes=self.n_docs) )
             
     def _generate_input_3(self, ID_list): 
         #inputs_netvec
@@ -294,6 +327,19 @@ class DataGenerator(tf.keras.utils.Sequence):
             x_batch[n,] = self.x2[i]
         return(x_batch) 
 
+    def _generate_input_123(self, ID_list):
+        x1_batch = np.empty((self.batch_size,self.x1.shape[1]), dtype=int)
+        x2_batch = np.empty((self.batch_size), dtype=int)
+        x3_batch = np.empty((self.batch_size,self.x2.shape[1]), dtype=int)
+        for n,i in enumerate(ID_list):
+            x1_batch[n] = self.x1[i]
+            x2_batch[n] = self.corpus_idx[i]
+            x3_batch[n,] = self.x2[i]
+        x2_batch_f = tf.keras.utils.to_categorical(x2_batch, num_classes=self.n_docs) 
+        return x1_batch,x2_batch_f,x3_batch
+        
+            
+        
     
     # def _generate_input_3(self, ID_list): 
     #     #inputs_netvec
