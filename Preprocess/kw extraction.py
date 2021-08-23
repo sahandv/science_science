19517c71078ccc44c2394a45d3c1b84ace14a0a3
Sahand <sahand.vahidnia@yahoo.com>
2021-08-23 23:29:22 +1000

    kw extraction final

--- Preprocess/kw extraction.py
+++ Preprocess/kw extraction.py
@@ -69,28 +69,68 @@ r.get_ranked_phrases()
 # =============================================================================
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
 from sciosci.assets import text_assets as kw
 from sciosci.assets import keyword_dictionaries as kd
 from gensim.parsing.preprocessing import strip_multiple_whitespaces
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
+from multiprocessing import Pool
+
 stops = ['a','an','we','result','however','yet','since','previously','although','propose','proposed','this','...']
 stop_words = list(set(stopwords.words("english")))+stops
 
 path = '/home/sahand/GoogleDrive/Data/'
 
-keywords = list(set(pd.read_csv(path+'Corpus/Taxonomy/AI kw merged US',names=['keyword'])['keyword'].values.tolist()))
+keywords = list(set(pd.read_csv(path+'Corpus/Taxonomy/TAI Taxonomy.csv',sep='===',names=['keyword'])['keyword'].values.tolist()))
+keywords = keywords+list(set(pd.read_csv(path+'Corpus/Taxonomy/1980-2019 300k n-gram author keyword taxonomy - 95percentile and cleaned.csv')['keywords'].values.tolist()))
+
 keywords = [x for x in keywords if len(x)>2]
+keywords = [kw.replace_british_american(strip_multiple_whitespaces(kw.replace_british_american(strip_multiple_whitespaces(keyword),kd.gb2us)),kd.gb2us) for keyword in keywords]
+keywords = [k.strip().lower() for k in keywords]
 keywords = np.array(keywords)
 
-abstracts = pd.read_csv(path+'Corpus/Dimensions AI unlimited citations/clean/abstract_title pure US',names=['abstract'])
-abstract = abstracts['abstract'][400]
-# keywords = [kw.replace_british_american(strip_multiple_whitespaces(kw.replace_british_american(strip_multiple_whitespaces(keyword),kd.gb2us)),kd.gb2us) for keyword in taxonomy]
-# keywords = np.array(keywords)
+pub_idx = pd.read_csv(path+'Corpus/Dimensions AI unlimited citations/clean/publication idx',names=['id'])[:]
+abstracts = pd.read_csv(path+'Corpus/Dimensions AI unlimited citations/clean/abstract_title pure US',names=['abstract'])[:]
+idx = abstracts.index
+abstracts = abstracts['abstract'].values.tolist()
 # pd.DataFrame(keywords).to_csv(path+'Corpus/Taxonomy/AI kw merged US',index=False,header=False)
-abstract = word_tokenize(abstract)
-abstract = [word for word in abstract if not word in stop_words] 
+# =============================================================================
+# abstract = word_tokenize(abstract)
+# abstract = [word for word in abstract if not word in stop_words] 
+# 
+# extraction = [word for word in abstract if word in keywords] 
+# matches = [keyword in abstract for keyword in keywords] 
+# selection = keywords[matches]
+# =============================================================================
+pool = Pool()
 
-extraction = [word for word in abstract if word in keywords] 
-matches = [keyword in abstract for keyword in keywords] 
-selection = keywords[matches]
+def extract(abstracts):
+    pubkeywords = []
+    errors = []
+    for i,abstract in tqdm(enumerate(abstracts),total=len(abstracts)):
+        try:
+            pubkeywords.append([x for x in keywords if x in abstract])
+        except:
+            pubkeywords.append([])
+            errors.append(i)
+    print('errors:'+str(errors))
+    return pubkeywords,errors
+
+extracted,errors = extract(abstracts)
+extracted = list(extracted)
+extracted_df = [str(list(row))[1:-1] for row in extracted]
+extracted_df = pd.DataFrame(extracted_df)
+extracted_df.index = idx
+extracted_df.to_csv(path+'Corpus/Dimensions AI unlimited citations/clean/keyword US p1',header=False)
+
+
+# =============================================================================
+# # concat multiple parts
+# =============================================================================
+extracted_df_b = pd.read_csv(path+'Corpus/Dimensions AI unlimited citations/clean/keyword US p2',index_col=0,header=None)
+extracted_df_b.columns = [0]
+extracted_df = extracted_df.append(extracted_df_b)
+pub_idx['data'] = extracted_df[0]
+pub_idx.to_csv(path+'Corpus/Dimensions AI unlimited citations/clean/keyword US',index=False)
+
