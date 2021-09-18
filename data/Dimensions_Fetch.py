2eb79f5e2cf54a4755d8c5a10a5fc187e2a35c26
sahandv <sahand.vahidnia@yahoo.com>
2021-09-19 01:24:36 +1000

    minor update
    Now saves data as json and gets category and concept as well

--- data/Dimensions_Fetch.py
+++ data/Dimensions_Fetch.py
@@ -12,6 +12,7 @@ from tqdm import tqdm
 import dimcli
 import pandas as pd
 import numpy as np
+import json
 dimcli.login(key="CC1FEFF8637149DBBA873831FCA4471F",endpoint="https://app.dimensions.ai")
 # dimcli --init
 # dimcli.login()
@@ -23,7 +24,7 @@ dsl = dimcli.Dsl()
 
 # data = dsl.query("""search publications for "black holes" return publications""")
 # data = dsl.query("""search publications for "black holes" return publications""", verbose=False)
-years = list(range(1961,2021))
+years = list(range(2018,2021))
 for year in tqdm(years):
     # year = 2016
     # data = dsl.query_iterative(r"""search publications for "\"artificial intelligence\"" where year="""+str(year)+
@@ -33,30 +34,40 @@ for year in tqdm(years):
     #                             times_cited + altmetric + reference_ids +  year + category_for + journal + 
     #                            proceedings_title + publisher + research_orgs]""")
     data = dsl.query_iterative(r"""search publications in full_data for "(\"machine learning\" OR \"artificial intelligence\" OR \"deep learning\")"  where year="""+str(year)+
-                               """ and (type="article" or type="proceeding") and times_cited = 0 and 
+                               """ and (type="article" or type="proceeding") and 
                                (category_for.name="01 Mathematical Sciences" or category_for.name="09 Engineering" or category_for.name="11 Medical and Health Sciences" or 
                                 category_for.name="17 Psychology and Cognitive Sciences" or category_for.name="06 Biological Sciences" or 
                                 category_for.name="15 Commerce, Management, Tourism and Services" or category_for.name="10 Technology")
                                and abstract is not empty and reference_ids is not empty and title is not empty and category_for is not empty
                                return publications 
-                               [id + authors + researchers + linkout + dimensions_url + doi + title + abstract + concepts + concepts_scores + 
-                                times_cited + altmetric + reference_ids +  year + category_for + journal + 
-                               proceedings_title + publisher + research_orgs] sort by altmetric desc""") #for "(\"machine learning\" OR \"artificial intelligence\" OR \"deep learning\")"
+                               [id + authors + researchers + linkout + dimensions_url + doi + title + abstract + concepts_scores + pages + funders + authors_count + mesh_terms + terms +
+                                times_cited + altmetric + reference_ids +  year + category_for + category_bra + category_hra + category_rcdc + journal +  category_ua + FOR_first + FOR 
+                               ] sort by altmetric desc""") #for "(\"machine learning\" OR \"artificial intelligence\" OR \"deep learning\")"
 
+# [id + authors + researchers + linkout + dimensions_url + doi + title + abstract + concepts_scores + pages + funders + authors_count + mesh_terms + terms +
+#  times_cited + altmetric + reference_ids +  year + category_for + category_bra + category_hra + category_rcdc + journal +  category_ua + FOR_first + FOR +
+# proceedings_title + publisher + research_orgs ]
     # =============================================================================
     # Process & Save
     # =============================================================================
     
-    data.json.keys()
-    len(data.publications)
-    print("We got", data.count_batch, "results out of", data.count_total)
-     # ps errors are printed out by default
-    print(data.errors_string)
+    output_address = '/home/sahand/GoogleDrive/Data/Corpus/Dimension All/'+str(year)+' dimensions AI articles-proceedings.json'
     
-    csv = pd.DataFrame(data.publications)
-    csv.to_csv('/mnt/16A4A9BCA4A99EAD/Dimensions/'+str(year)+' dimensions AI articles-proceedings cited>0.csv')
+    # data_json = json.dumps(data.publications)
+
+    with open(output_address, 'w') as json_file:
+        json.dump(data.publications, json_file)
     
-    del csv
+    # data.json.keys()
+    # len(data.publications)
+    # print("We got", data.count_batch, "results out of", data.count_total)
+    #  # ps errors are printed out by default
+    # print(data.errors_string)
+    
+    # csv = pd.DataFrame(data.publications)
+    # csv.to_csv('/home/sahand/GoogleDrive/Data/Corpus/Dimension All/'+str(year)+' dimensions AI articles-proceedings cited>0.csv')
+    
+    # del csv
     del data
     gc.collect()
     
@@ -76,10 +87,10 @@ data = dsl.query_iterative(r"""search publications in full_data for "(\"machine 
                                 category_for.name="15 Commerce, Management, Tourism and Services" or category_for.name="10 Technology")
                                and abstract is not empty and reference_ids is not empty and title is not empty and category_for is not empty
                                return publications 
-                               [id + authors + researchers + linkout + dimensions_url + doi + title + abstract + 
-                                times_cited + altmetric + reference_ids +  year + category_for + journal + 
+                               [id + authors + researchers + linkout + dimensions_url + doi + title + abstract 
+                                times_cited + altmetric + reference_ids + references + year + category_for + journal + 
                                proceedings_title + publisher + research_orgs] sort by altmetric desc""") # for "\"machine learning\""  and times_cited > 0 
-                               
+           
 data.json.keys()
 len(data.publications)
 print("We got", data.count_batch, "results out of", data.count_total)
@@ -89,6 +100,13 @@ print(data.errors_string)
 csv = pd.DataFrame(data.publications)
 csv.to_csv('/mnt/16A4A9BCA4A99EAD/Dimensions/dimensions AI DL ML all articles-proceedings.csv')
 
+# =============================================================================
+# Read for test
+# =============================================================================
+address = '/home/sahand/GoogleDrive/Data/Corpus/Dimension All/2010 dimensions AI articles-proceedings.json'
+with open(address) as f:
+    data = json.load(f)
+df = pd.DataFrame(data)
 # =============================================================================
 # Drop randomly to reduce dataset size
 # =============================================================================
